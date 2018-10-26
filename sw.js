dcd302f080ce4b371d7ca10e64b9578eca420021
ansinlaw <imyentsen@gmail.com>
2018-10-26 04:40:30 +0300

    Initial commit

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-fcdec84c63001f050574.js"
+    "url": "webpack-runtime-af82c1ea83df5f85bd8f.js"
   },
   {
     "url": "app-973e44235a3c494d1e00.js"
@@ -36,17 +36,17 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "34247a194071bdc13e32daccf08ee00e"
+    "revision": "de74d8fe4e1036476647002688dc8012"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "db575a0f777c080767f35de23e4a2a78"
+    "revision": "a0196039969c391312abf0d8eb728dc2"
   },
   {
-    "url": "1.aacb0a6330b10b794bdd.css"
+    "url": "1.c8f0b6308bca4c189f5c.css"
   },
   {
-    "url": "component---src-pages-index-js.01299335d0342ba75e0c.css"
+    "url": "component---src-pages-index-js.f6bc7ccad27330089392.css"
   },
   {
     "url": "0.5dd481189c9f6bdb60c8.css"
@@ -55,7 +55,7 @@ self.__precacheManifest = [
     "url": "12.3af4c4eb1996eb603a75.css"
   },
   {
-    "url": "component---src-pages-index-js-b9d7ced7449146e699c2.js"
+    "url": "component---src-pages-index-js-a6e1ab20951eaf6d622f.js"
   },
   {
     "url": "2-dcd6d3d7c078082957f6.js"
@@ -67,7 +67,7 @@ self.__precacheManifest = [
     "url": "12-8843d90c486b6022735e.js"
   },
   {
-    "url": "1-17190f1e322ff88891d3.js"
+    "url": "1-0506689c5f3177adc64f.js"
   },
   {
     "url": "static/d/173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E.json",
