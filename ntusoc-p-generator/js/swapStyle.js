4c73d492340e046a39de000591c8f9f11d71d472
ansinlau <ansinlau@dhcp-86-92.eduroam.aalto.fi>
2019-04-25 22:42:56 +0300

    ntu soc poster 2

--- ntusoc-p-generator/js/swapStyle.js
+++ ntusoc-p-generator/js/swapStyle.js
@@ -3,9 +3,9 @@ $(function() {
     var randomColor = (function lol(m, s, c) {
         return s[m.floor(m.random() * s.length)] +
             (c && lol(m, s, c - 1));
-    })(Math, '89ABCDE', 4);
+    })(Math, '789ABCD', 4);
 
-    var gradient = "radial-gradient(circle at top left, #" + randomColor +" 20%, rgba(255,255,255,0.5))";
+    var gradient = "radial-gradient(circle at top left, #" + randomColor +" 25%, rgba(40,40,40,0.3) 90%, rgba(0,0,0,0.0) 95%)";
 
     $(".bg-svg").css({
         backgroundImage: gradient,
