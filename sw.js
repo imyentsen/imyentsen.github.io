d50075f503546fb50d45afb9e9072c1bf0a11dfc
imyentsen <imyentsen@gmail.com>
2020-10-06 12:25:27 +0800

    bug fixes

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9556f1277edb9fea8cdf.js"
+    "url": "webpack-runtime-9f60925a9c4a261ad103.js"
   },
   {
     "url": "framework-3c3432940b412e047373.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b85b6dbfd65cb81b9bd23519c929e50e"
+    "revision": "b9f3da45a3e3de0cf364d42f45e9a2f1"
   },
   {
     "url": "polyfill-6d0b758d21a2d8b2881f.js"
