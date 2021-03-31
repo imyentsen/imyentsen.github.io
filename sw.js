64e06c5a001805819cd12fc52872747088c1470b
imyentsen <imyentsen@gmail.com>
2021-03-31 17:03:07 +0800

    update of a new work, Taqun, and a few fixes on UI bugs

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-90cd9a381a276b15f18c.js"
+    "url": "webpack-runtime-66f864507e1c09735b16.js"
   },
   {
     "url": "framework-1d1c5cf86b4657b85854.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "131d89de8167a9b0ad903611b6482a00"
+    "revision": "aac5f69b0e798604fd5e7dc4949e43df"
   },
   {
     "url": "polyfill-9e85c4bee99a7df35b0b.js"
